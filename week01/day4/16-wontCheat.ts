502b01298c3ceed89ae20f1c57a138e09a00f843
fehersanyi <papahadacow@gmail.com>
2018-02-15 14:33:27 +0100

    some progress added

---
+++ week01/day4/16-wontCheat.ts
@@ -0,0 +1,4 @@
+'use strict';
+for (let i: number = 1 ; i <= 100; i++) {
+    console.log('I won\'t cheat on the exam!');
+}
---
+++ week01/day4/18-multiplicationTable.ts
@@ -0,0 +1,7 @@
+'use strict';
+let number: number = 15;
+let mult: number = 0;
+while (mult <=10){
+    console.log(`${number} x ${mult} = ${number * mult}`);
+    mult++;
+}
