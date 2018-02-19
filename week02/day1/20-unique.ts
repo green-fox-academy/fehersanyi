56cdc343cf1590b272d1fa62ba19c48d76c5809e
fehersanyi <papahadacow@gmail.com>
2018-02-19 16:30:39 +0100

    task 20 modified

--- week02/day1/20-unique.ts
+++ week02/day1/20-unique.ts
@@ -1,5 +1,5 @@
 'use strict';
 let myArray: number[] = [1, 11, 34, 11, 52, 61, 1, 34];
-let makeUnique = (a) => a.filter((el, i, self) => self.indexOf(el) === i);
+let makeUnique = (inPut) => inPut.filter((element, index, self) => self.indexOf(element) === index);
 
 console.log(makeUnique(myArray));
