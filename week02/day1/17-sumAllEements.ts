25d6df00686675efd05ea3a64a271281d2cd8a09
fehersanyi <papahadacow@gmail.com>
2018-02-19 16:43:47 +0100

    upgraded task 17

--- week02/day1/17-sumAllEements.ts
+++ week02/day1/17-sumAllEements.ts
@@ -1,3 +1,7 @@
 'use strict';
 let ai: number[] = [3, 4, 5, 6, 7];
-console.log(ai[0]+ai[1]+ai[2]+ai[3]+ai[4]);
+let sum: number = 0;
+ai.forEach(function(value: number){
+    sum += value;
+})
+console.log(sum);
