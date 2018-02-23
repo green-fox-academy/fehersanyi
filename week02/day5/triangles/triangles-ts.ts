e619ae7401a3e6bebf9bb75c119200d0be55cf1e
fehersanyi <papahadacow@gmail.com>
2018-02-23 11:35:31 +0100

    four xp here i come

--- week02/day5/triangles/triangles-ts.ts
+++ week02/day5/triangles/triangles-ts.ts
@@ -2,50 +2,28 @@
 
 const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
 const ctx = canvas.getContext('2d');
-/*
-function drawMeALine(mx, my, lx, ly) {
-    ctx.beginPath();
-    ctx.moveTo(mx, my);
-    ctx.lineTo(lx, ly);
-    ctx.stroke();
+//(Math.cbrt(3)/2)*10; //8.66
+let originPoint1: number = 200;
+let originPoint2: number = 200;
+let startPoints: number = originPoint1;
+let startPoints2: number = originPoint2;
+let rowNumber: number = 5;
+function drawThatTriangle (x: number, y: number) {
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+        ctx.lineTo(x-5, y+Math.cbrt((3)/2)*10);
+        ctx.lineTo(x+5,y+Math.cbrt((3)/2)*10);
+        ctx.lineTo(x, y);
+        ctx.stroke();
 }
-let mHor: number;
-let mVer: number;
-let lHor: number;
-let lVer: number;
-let x = Math.sqrt;*/
-
-
-ctx.beginPath();
-    ctx.moveTo(20, 0);
-    ctx.lineTo(30, Math.sqrt(2)*10);
-    ctx.lineTo(10, Math.sqrt(2)*10);
-    ctx.lineTo(20, 0);
-    ctx.stroke();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-function makeTriangle() {
-    ctx.beginPath();
-    ctx.moveTo(200, 0);
-    ctx.lineTo(207.5, 10);
-    ctx.lineTo(192.5, 10);
-    ctx.lineTo(200, 0);
-    ctx.stroke();
+drawThatTriangle(startPoints, startPoints2);
+for(let j: number = 0; j < rowNumber; j++) {
+  let myCorr: number = j * rowNumber;
+  for(let i: number = 0; i < rowNumber-j; i++) {
+    drawThatTriangle(originPoint1, originPoint2);
+    originPoint1 -= 5;
+    originPoint2 += Math.cbrt((3)/2)*10;
+  }
+  originPoint1 += 30 - myCorr;
+  originPoint2 = (200 + Math.cbrt((3)/2)*10) + (j *Math.cbrt((3)/2)*10);
 }
-makeTriangle();*/
-
-
