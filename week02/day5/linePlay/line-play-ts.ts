43b0dc4709e59b813305b3a18478cff4d4a8a548
fehersanyi <papahadacow@gmail.com>
2018-02-23 15:01:06 +0100

    i can't even

--- week02/day5/linePlay/line-play-ts.ts
+++ week02/day5/linePlay/line-play-ts.ts
@@ -6,8 +6,8 @@ const ctx = canvas.getContext('2d');
 function purpleLines(xStart: number, yStart: number, xEnd: number, yEnd: number) {
     ctx.strokeStyle = 'purple';
     ctx.beginPath();
-    ctx.moveTo(xStart, yStart);
-    ctx.lineTo(xEnd, yEnd);
+    ctx.moveTo(xStart/4, yStart/4);
+    ctx.lineTo(xEnd/4, yEnd/4);
     ctx.stroke();
 }
 
@@ -15,8 +15,8 @@ function purpleLines(xStart: number, yStart: number, xEnd: number, yEnd: number)
 function greenLines(xStart: number, yStart: number, xEnd: number, yEnd: number) {
     ctx.strokeStyle = 'green';
     ctx.beginPath();
-    ctx.moveTo(xStart, yStart);
-    ctx.lineTo(xEnd, yEnd);
+    ctx.moveTo(xStart/4, yStart/4);
+    ctx.lineTo(xEnd/4, yEnd/4);
     ctx.stroke();
 }
 
@@ -34,10 +34,24 @@ for(let i: number = 0; i < 18; i++) {
 
 
     purpleLines(horStart, verStart, horEnd, verEnd);
-
+    purpleLines(horStart+400, verStart+400, horEnd+400, verEnd+400);
+    purpleLines(horStart+800, verStart+800, horEnd+800, verEnd+800);
+    purpleLines(horStart+1200, verStart+1200, horEnd+1200, verEnd+1200);
+    purpleLines(horStart+400, verStart, horEnd+400, verEnd);
+    purpleLines(horStart+400, verStart+1200, horEnd+400, verEnd+1200);
+    purpleLines(horStart+800, verStart, horEnd+800, verEnd);
+    purpleLines(horStart+800, verStart+400, horEnd+800, verEnd+400);
+    purpleLines(horStart+1200, verStart, horEnd+1200, verEnd);
+    purpleLines(horStart+1200, verStart+400, horEnd+1200, verEnd+400);
+    purpleLines(horStart+1200, verStart+800, horEnd+1200, verEnd+800);
+    purpleLines(horStart, verStart+400, horEnd, verEnd+400);
+    purpleLines(horStart, verStart+800, horEnd, verEnd+800);
+    purpleLines(horStart, verStart+1200, horEnd, verEnd+1200);
+    purpleLines(horStart+400, verStart+800, horEnd+400, verEnd+800);
+    purpleLines(horStart+800, verStart+1200, horEnd+800, verEnd+1200);
 }
 
-// yold csikok
+// zold csikok
 for(let i: number = 0; i < 18; i++) {
     horStart = 0;
     verStart = 370 - ((i * 20) - 2);
@@ -46,4 +60,19 @@ for(let i: number = 0; i < 18; i++) {
 
 
     greenLines(horStart, verStart, horEnd, verEnd);
+    greenLines(horStart+400, verStart+400, horEnd+400, verEnd+400);
+    greenLines(horStart+800, verStart+800, horEnd+800, verEnd+800);
+    greenLines(horStart+1200, verStart+1200, horEnd+1200, verEnd+1200);
+    greenLines(horStart+400, verStart, horEnd+400, verEnd);
+    greenLines(horStart+400, verStart+1200, horEnd+400, verEnd+1200);
+    greenLines(horStart+800, verStart, horEnd+800, verEnd);
+    greenLines(horStart+800, verStart+400, horEnd+800, verEnd+400);
+    greenLines(horStart+1200, verStart, horEnd+1200, verEnd);
+    greenLines(horStart+1200, verStart+400, horEnd+1200, verEnd+400);
+    greenLines(horStart+1200, verStart+800, horEnd+1200, verEnd+800);
+    greenLines(horStart, verStart+400, horEnd, verEnd+400);
+    greenLines(horStart, verStart+800, horEnd, verEnd+800);
+    greenLines(horStart, verStart+1200, horEnd, verEnd+1200);
+    greenLines(horStart+400, verStart+800, horEnd+400, verEnd+800);
+    greenLines(horStart+800, verStart+1200, horEnd+800, verEnd+1200);
 }
--- week02/day5/superHexagon/hexagon-ts.ts
+++ week02/day5/superHexagon/hexagon-ts.ts
@@ -1,17 +1,28 @@
 'use strict'
 const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
 const ctx = canvas.getContext('2d');
-
+let backUpPoint1: number = 190;
+let backUpPoint2: number = 200;
 let originPoint: number = 190;
 let originPoint2: number = 200;
+let hexSize: number = 2;
 function drawDaHex(x: number, y: number){
   ctx.beginPath();
   ctx.moveTo(x, y);
-  ctx.lineTo(x + 20, y);
-  ctx.lineTo(x + 30, y + Math.sqrt(2) / 2 * 20);
-  ctx.lineTo(x + 20, y + Math.sqrt(2) * 20);
- /* ctx.lineTo();
-  ctx.lineTo(x, y);*/
+  ctx.lineTo(x + 16, y);
+  ctx.lineTo(x + Math.sqrt(2) * 16, y + Math.sqrt(2) / 2 * 16);
+  ctx.lineTo(x + 16, y + Math.sqrt(2) * 16);
+  ctx.lineTo(x, y + Math.sqrt(2) * 16 );
+  ctx.lineTo((x - Math.sqrt(2) * 8) + 5, y + Math.sqrt(2) / 2 * 16 );
+  ctx.lineTo(x, y);
   ctx.stroke();
 }
-drawDaHex(originPoint, originPoint2);
+for(let j: number = 0; j < hexSize; j++) {
+  for(let i: number = 0; i < hexSize + 1; i++){
+    drawDaHex(originPoint, originPoint2);
+    originPoint -= (Math.sqrt(2) * 10 + 15);
+    originPoint2 += Math.sqrt(2) / 2 * 20;
+  }
+  originPoint = backUpPoint1 + (40 * (j + 0.75));
+  originPoint2 = backUpPoint2 + ((Math.sqrt(2) / 2 * 20) + (Math.sqrt(2)));
+}
