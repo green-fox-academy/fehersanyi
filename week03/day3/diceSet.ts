8bc3eca8e8813de78f2c58aeca22239d722aa5a1
fehersanyi <papahadacow@gmail.com>
2018-02-28 13:56:39 +0100

    cant even

---
+++ week03/day3/diceSet.ts
@@ -0,0 +1,64 @@
+class DiceSet {
+  /**    You have a Dice class which has 6 dice */
+  /**   You can roll all of them with roll() */
+  /**   Check the current rolled numbers with getCurrent() */
+  /**   You can reroll with reroll() */
+  /**   Your task is to get where all dice is a 6 */
+      dices: number[];
+      readonly numOfDices: number = 6;
+  
+      roll(): number[] {
+          this.dices = [];
+          for(var i = 0; i < this.numOfDices; i++) { 
+              this.dices.push(Math.floor(Math.random() * 6 + 1));
+          }
+          return this.dices;
+      }
+  
+      reroll(index?: number) {
+          if(index == undefined) {
+              for(var i = 0; i < this.numOfDices; i++) { 
+                  this.dices[i] = Math.floor(Math.random() * 6 + 1);
+              }
+          } else {
+              this.dices[index] = Math.floor(Math.random() * 6 + 1);
+          }
+      }
+      
+      getCurrent(index?: number) {
+          if(index == undefined) {
+              for(var i = 0; i < this.numOfDices; i++) { 
+                  console.log(this.dices[i]);
+              }
+          } else {
+              console.log(this.dices[index]);
+          }       
+      }
+  }
+  
+  let myDice = new DiceSet();
+  // myDice.roll();
+  // myDice.getCurrent();
+  // console.log("------------------");
+  // myDice.reroll();
+  // myDice.getCurrent();
+  // console.log("------------------");
+  // myDice.getCurrent(5);
+  // myDice.reroll();
+  // myDice.getCurrent();
+  // console.log("------------------");
+  // myDice.reroll(4);
+  // myDice.getCurrent();
+let i: number = 0;
+ let diceOnTable: number[] = [];
+  while (myDice[i] !== 6) {
+    i++;
+    myDice.roll();
+      if (myDice.getCurrent(i) !== 6) {
+        diceOnTable.push(this.dices(0))
+     }
+    
+    
+
+  }
+  myDice.getCurrent();
