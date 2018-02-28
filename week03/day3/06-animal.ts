f6e089a6ffdac38f1af0d23b959ed70b2ad54d0e
fehersanyi <papahadacow@gmail.com>
2018-02-28 16:26:53 +0100

    animals are playing nicely

---
+++ week03/day3/06-animal.ts
@@ -0,0 +1,25 @@
+'use strict';
+
+class Animal {
+  hunger: number = 50;
+  thirst: number = 50;
+
+  constructor() {
+
+  }
+
+  eat() {
+    this.hunger--;
+  }
+  drink() {
+    this.thirst--;
+  }
+  play() {
+    this.thirst++;
+    this.hunger++;
+  }
+}
+
+let tiger = new Animal;
+tiger.play();
+console.log(tiger.hunger);
