aef6aad6c3cd5f0e1275755cfd718bd784eb4665
fehersanyi <papahadacow@gmail.com>
2018-02-28 19:39:53 +0100

    lottery madafaka

--- week03/day2/13-lottery.ts
+++ week03/day2/13-lottery.ts
@@ -6,19 +6,27 @@ let lotteryData: string = fs.readFileSync(myFile, 'utf-8');
 let dataLine: any [] = lotteryData.split('\r\n');
 let dataEnd: any [] = [];
 let cheesyHotDog: any[] = [];
-let numberOfHotDogs: any[] = []
 for (let i: number = 0; i < dataLine.length; i++) {
   dataEnd = dataLine[i].split(';');
   for (let j: number = 11; j < dataEnd.length; j++) {
     cheesyHotDog.push(dataEnd[j]);
   }
 }
-cheesyHotDog = cheesyHotDog.sort();
-for (let k: number = 0; k < cheesyHotDog.length; k++) {
-  if (cheesyHotDog[k].value === cheesyHotDog[k-1].value) {
-    numberOfHotDogs[k].push(cheesyHotDog)
+let yummiHotDog: {} = {}
+for(let i: number = 0; i < cheesyHotDog.length; i++) {
+  if (yummiHotDog[cheesyHotDog[i]] === undefined) {
+    yummiHotDog[cheesyHotDog[i]] = 0;
   }
+  yummiHotDog[cheesyHotDog[i]]++;
 }
-console.log(cheesyHotDog);
+let biggestOccurance: number[] = Object.values(yummiHotDog).sort().reverse().splice(1,5);
+let finalfinals: any [] = [];
+Object.keys(yummiHotDog).forEach(function(cheesyHotDog) {
+  if(biggestOccurance.indexOf(yummiHotDog[cheesyHotDog]) > -1) {
+    finalfinals.push(cheesyHotDog);
+  }
+})
+console.log(finalfinals);
 }
+
 dataAnalize('lottery.cvs');
