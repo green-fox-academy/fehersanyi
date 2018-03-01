99491d7b5553f261e7befee884308be7e583943d
fehersanyi <papahadacow@gmail.com>
2018-03-01 10:21:04 +0100

    summed em digits

---
+++ week03/day4/03-sumDigit.ts
@@ -0,0 +1,13 @@
+'use strict';
+
+function sumDigits(n: number) {
+  let remainder = n % 10;
+  let sum = remainder;
+  if(n >= 10) {
+      let rest = Math.floor(n / 10);
+      sum += sumDigits(rest); 
+  }
+  return sum;
+}
+
+console.log(sumDigits(21));
