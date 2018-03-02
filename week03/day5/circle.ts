103939b836635097402a2c6f6233e0095dc18e72
fehersanyi <papahadacow@gmail.com>
2018-03-02 13:50:13 +0100

    fixed the israeli-palestinian conflict

--- week03/day5/circle.ts
+++ week03/day5/circle.ts
@@ -3,6 +3,20 @@
 const canvas = document.querySelector('.main-canvas') as sTMLCanvasElement;
 const ctx = canvas.getContext('2d');
 
-function circle(x, y, s) {
+function circle(x: number, y: number, size: number, d: number, thisIsPI: number) {
 
-ctx.strokeStyle = `hsl(${Math.random() * 360}, 100%, 50%)`;  
+  ctx.strokeStyle = `hsl(${Math.random() * 360}, 100%, 50%)`;  
+
+  ctx.beginPath();
+  ctx.arc(x, y, size, d, thisIsPI);
+  ctx.stroke();
+
+  if(size > 10) {
+    circle(x, y - size / 2, size / 2, d, thisIsPI);
+    circle(x - size * Math.sqrt(3) / 4, y + size / 4, size / 2, d, thisIsPI);
+    circle(x + size * Math.sqrt(3) / 4, y + size / 4, size / 2, d, thisIsPI);
+
+  }
+}
+
+circle(300, 300, 300, 0, Math.PI * 2);
