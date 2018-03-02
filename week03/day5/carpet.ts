000775d11f98dadd0ccf8fd0d39c6f1b69a79714
fehersanyi <papahadacow@gmail.com>
2018-03-02 12:38:04 +0100

    I am Human

--- week03/day5/carpet.ts
+++ week03/day5/carpet.ts
@@ -5,18 +5,20 @@ const ctx = canvas.getContext('2d');
 
 function demCarpet(x: number, y: number, s: number) {
 
-//ctx.fillStyle = `hsl(${Math.random() * 360}, 100%, 50%)`;
+  ctx.fillStyle = `hsl(${Math.random() * 360}, 100%, 50%)`;
 
   ctx.fillRect(x, y, s, s)
 
   if(s > 5) {
     //ez jo
-    demCarpet(x - (2 * s / 3), y / 3, s / 3);
-    demCarpet((x * 3 + s) / 3, y / 3, s / 3);
-    demCarpet((x + s + (s / 3)), y / 3, s / 3);
-    demCarpet(x - (2 * s / 3), y + s / 3, s / 3);
-    demCarpet( x + s + (s / 3), y + s / 3, s / 3);
-    demCarpet(x - (2 * s / 3), y + s + (s / 3), s / 3);
+    demCarpet(x - s / 3 * 2, y - s / 3 * 2, s / 3);
+    demCarpet(x + s / 3, y - s / 3 * 2, s / 3);
+    demCarpet(x + s + s / 3, y - s / 3 * 2, s / 3);
+    demCarpet(x - s / 3 * 2, y + s / 3, s / 3);
+    demCarpet(x + s + s / 3, y + s / 3, s / 3);
+    demCarpet(x + s + s / 3, y + s + s / 3, s / 3);
+    demCarpet(x - s / 3 * 2, y + s + s / 3, s / 3);
+    demCarpet(x + s / 3, y + s + s / 3, s / 3);
   }
 }
 demCarpet(200, 200, 200)
