514441860dcda3a71525df6ed690ae016be6dea6
fehersanyi <papahadacow@gmail.com>
2018-03-06 15:30:19 +0100

    type and status aviable

--- week04/day2/airCraft.ts
+++ week04/day2/airCraft.ts
@@ -3,6 +3,7 @@
 class AirCraft {
 	ammo: number = 0;
 	baseDamage: number;
+	maxAmmo: number;
 	constructor() {
 		this.ammo;
 	}
@@ -13,30 +14,56 @@ class AirCraft {
 		return dmg;
 	}
 
-	refill() {}
+	refill() {
+		return this.ammo += this.maxAmmo - this.ammo;
+	}
+
+	getType() {}
+
+	getStatus() {
+		return `Type ${this.getType()}, Ammo:${this.ammo}, Base damage:${this.baseDamage}, All damage: ${this.ammo * this.baseDamage}`
+	}
 
 }
 
 class F16 extends AirCraft {
 	maxAmmo: number = 8;
 	baseDamage: number = 30; 
-	constructor(ammo: number) {
+	constructor() {
 		super();
-		this.ammo = ammo
 	}
-
-	refill() {
-		return this.ammo += this.maxAmmo - this.ammo;
+	getType() {
+		return 'F16'
 	}
 }
 
 class F35 extends AirCraft {
-    
+	maxAmmo: number = 12;
+	baseDamage: number = 50;
+	constructor() {
+		super();
+	}
+
+	getType() {
+		return 'F35'
+	}
+
 }
 
-let test = new F16(0);
+let test = new F16();
 console.log(test.ammo);
 test.refill();
 console.log(test.ammo);
 console.log(test.fight());
 console.log(test.ammo);
+
+let test2 = new F35();
+console.log(test2.ammo);
+test2.refill();
+console.log(test2.ammo);
+console.log(test2.fight());
+console.log(test2.ammo);
+console.log(test2.getStatus());
+test2.refill();
+console.log(test2.getStatus());
+console.log(test.getType());
