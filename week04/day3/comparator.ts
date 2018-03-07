7279a99eb607004d000e64603ed468988cf9b36f
fehersanyi <papahadacow@gmail.com>
2018-03-07 14:09:10 +0100

    solved death itself

--- week04/day3/comparator.ts
+++ week04/day3/comparator.ts
@@ -1,6 +1,6 @@
 'use strict';
 import { Domino } from '../../week03/day3/domino'
-
+import { Thing } from '../../week03/day3/thing'
 let dominoes: Domino[] = [];
 dominoes.push(new Domino(5, 2));
 dominoes.push(new Domino(4, 6));
@@ -9,6 +9,19 @@ dominoes.push(new Domino(6, 7));
 dominoes.push(new Domino(2, 4));
 dominoes.push(new Domino(7, 1));
 
-console.log(dominoes.sort(function (a: Domino , b: Domino): number {
+console.log(dominoes.sort(function (a: Domino , b: Domino) : number {
+  return a.compareTo(b);
+}));
+
+let things: Thing[] = [];
+things.push(new Thing('giveUp'));
+things.push(new Thing('killMyself'));
+things.push(new Thing('solve the problem'));
+things.push(new Thing('live a happy life'));
+things[0].complete();
+things[2].complete();
+
+
+console.log(things.sort(function (a: Thing , b: Thing) : number {
   return a.compareTo(b);
 }));
