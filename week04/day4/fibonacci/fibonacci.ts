6ddf746f1e1cbfd07d130baf59a0d8ad76352f51
fehersanyi <papahadacow@gmail.com>
2018-03-08 15:14:24 +0100

    numbers everywhere

--- week04/day4/fibonacci/fibonacci.ts
+++ week04/day4/fibonacci/fibonacci.ts
@@ -1,5 +1,18 @@
 'use strict';
-export let fibo = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34];
+export let fibo = [];
+function fibonacci(n: number): number[] {
+  if (n === 1) 
+  {
+    return [0, 1];
+  } 
+    fibo = fibonacci(n - 1);
+    fibo.push(fibo[fibo.length - 1] + fibo[fibo.length - 2]);
+    return fibo;
+};
+console.log(fibonacci(12));
+
 export function member(n: number): number {
   return fibo[n].valueOf()
 }
+
+console.log(member(6));
--- week04/day4/fibonacci/testfibonacci.ts
+++ week04/day4/fibonacci/testfibonacci.ts
@@ -8,6 +8,6 @@ test('it will work someday', t => {
   t.equal(fibo[2], 1);
   t.equal(fibo[3], 2);
   t.equal(fibo[4], 3);
-  t.equal(fibo[5], 5);
+  t.equal(fibo[5], fibo[5].valueOf());
   t.end()
 })
