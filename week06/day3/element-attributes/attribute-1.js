771f1312d6484c486429d718f6c510c7f541f813
fehersanyi <papahadacow@gmail.com>
2018-03-28 13:12:08 +0200

    i think im done with attributes stuff

---
+++ week06/day3/element-attributes/attribute-1.js
@@ -0,0 +1,7 @@
+let url = document.querySelector('img');
+console.log(url.getAttribute('src'));
+url.setAttribute('src', 'llama.jpg');
+let link = document.querySelector('a');
+link.setAttribute('href', 'https://www.greenfoxacademy.com/')
+let btn = document.querySelector('.this-one');
+btn.innerHTML = 'Don\'t click me!';
