479e453775730f2363df09c443f3afa5b8225994
fehersanyi <papahadacow@gmail.com>
2018-03-28 11:17:28 +0200

    i think it is good

---
+++ week06/day3/queryselector/queryselector.js
@@ -0,0 +1,5 @@
+let king = document.getElementsByClassName('asteroid');
+console.log(king[0]);
+const businessLamp = [ document.querySelector('.b329') ,
+document.querySelectorAll('div.asteroid')];
+console.log(businessLamp);
