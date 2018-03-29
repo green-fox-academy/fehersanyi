303f338e442cc049c4646e05c14c18aec9722c3c
fehersanyi <papahadacow@gmail.com>
2018-03-29 13:29:31 +0200

    we are looking good

---
+++ week06/day4/build/scripts/main.js
@@ -0,0 +1,15 @@
+let imageSources = [
+  "../../src/img/doge1.png",
+  "../../src/img/doge2.png",
+  "../../src/img/doge3.png",
+  "../../src/img/meme1.png",
+  "../../src/img/meme2.png",
+  "../../src/img/meme3.png"]
+
+let pics = document.querySelectorAll(".thumbnails img");
+let viewer = document.querySelector(".viewer");
+let btn1 =document.querySelector(".button1");
+let btn2 =document.querySelector(".button2");
+btn1.addEventListener('click', () => {
+
+});
