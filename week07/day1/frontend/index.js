3794860a7b5007050dc0860735d844cb871f54f0
fehersanyi <papahadacow@gmail.com>
2018-04-03 15:22:06 +0200

    weekly  commit

--- week07/day1/frontend/index.js
+++ week07/day1/frontend/index.js
@@ -19,11 +19,11 @@
    const recieved = req.query.input;
    if (recieved === undefined) {
      res.json({
-       error: 'please give me something!'
+       error: 'Please provide an input!'
      });
    } else {
      res.json({
-       recieved: recieved,
+       received: parseInt(recieved),
        result: doubling(recieved)
      });
    }
@@ -33,26 +33,28 @@
    return i * 2;
  };
 
- app.get('/greet', (req, res) => {
+ app.get('/greeter', (req, res) => {
    const name = req.query.name;
    const title = req.query.title;
-   if (name === undefined || title === undefined) {
-     res.status(404);
+   if (name === undefined) {
      res.json({
-       error: 'Please provide a name and a title!'
+       error: 'Please provide a name!'
      });
+   } else if (title === undefined) {
+     res.json({
+       error: 'Please provide a title!'
+     })
    } else {
      res.json({
-       welcome_message: `Oh, hi there ${name}, my dear ${title}`
+       welcome_message: `Oh, hi there ${name}, my dear ${title}!`
      });
    };
  });
 
 
- app.get('/append', (req, res) => {
-   const appendable = req.query.appendable
+ app.get('/appenda/:appendable', (req, res) => {
+   const appendable = req.params.appendable;
    if (appendable === undefined) {
-     res.status(404);
      res.json({
        error: 'Please provide a word!'
      });
@@ -68,27 +70,55 @@
  }
 
  app.post('/dountil/:operator', (req, res) => {
-   let n = req.query.until;
+   let until = req.query.until;
    if (req.params.operator === 'sum') {
      res.json(
-       summer(n)
+       summer(until)
      );
    };
-   if(req.params.operator === 'factor') {
+   if (req.params.operator === 'factor') {
      res.json(
-       factor(n)
+       factor(until)
      );
    };
  });
 
  let summer = (n) => {
    let result = 0;
-   for (let i = 0; i <= until; i++) {
+   for (let i = 0; i <= n; i++) {
      result += i;
    };
    return result;
  };
 
- let factor = () => {
-   
+ let factor = (n) => {
+   let value = 1;
+   for (let i = 0; i < n; i++) {
+     value = value * (n - i)
+   }
+   return value;
  };
+
+
+ app.post('/arrays/:operation', (req, res) => {
+  let what = req.params.operation;
+  let numbers = req.query.numbers
+  if(what === 'sum') {
+    res.json(sum(numbers))
+  }
+  else if(what === 'multiply') {}
+  else if(what === 'double') {}
+  else if(what === undefined) {
+    res.json({
+      error: 'Please provide what to do with the numbers!'
+    });
+  };
+ });
+
+ let sum = (array) => {
+   let value = 0;
+   for(let i = 0; i < array.lenght; i++) {
+    value += array[i];
+   }
+   return value;
+ }
