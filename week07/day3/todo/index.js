60948959c31d7d8a34b70488d9eb4e724972855b
fehersanyi <papahadacow@gmail.com>
2018-04-05 10:30:58 +0200

    now i have things to do

--- week07/day3/todo/index.js
+++ week07/day3/todo/index.js
@@ -2,18 +2,20 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-// set the view engine to ejs
 app.set('view engine', 'ejs');
-// home page
+
+const todos = [
+  'get up',
+  'survive',
+  'go back to bed',
+];
+
 app.get('/', (req, res) => {
-  const name = req.query.name;
-  // render `home.ejs`
   res.render('home', {
-    name: name || 'Guest'
+    todos: todos
   });
 });
 
-// start express app on port 3000
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
