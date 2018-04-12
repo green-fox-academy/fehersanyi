443db67a2e2d5316d1d5833cfba2a739cf8c4af3
fehersanyi <papahadacow@gmail.com>
2018-04-12 16:59:41 +0200

    index frontend ready

--- reddit-project/index.js
+++ reddit-project/index.js
@@ -3,8 +3,13 @@ const app = express();
 const PORT = 3000;
 
 const postController = require('./controller/controll');
-
 app.use(express.json());
+app.set('view engine', 'ejs');
+app.use('/static', express.static('static'));
+
+app.get('/', (req, res) => {
+  res.render('index')
+});
 
 app.post('/posts', postController.addRedditPost);
 app.get('/posts', postController.getRedditPost);
