108b85a11cf011e57574e0bd0ede5d569494fc91
fehersanyi <papahadacow@gmail.com>
2018-04-16 14:01:41 +0200

    task 6

--- week09/day1/proise/program.js
+++ week09/day1/proise/program.js
@@ -2,8 +2,12 @@
 
 require('es6-promise');
 
-let promise = new Promise((fulfill, reject) => {
-		fulfill('PROMISE VALUE')
-	})
-	.then(console.log)
-console.log('MAIN PROGRAM')
+// let promise = new Promise((fulfill, reject) => {
+// 		fulfill('PROMISE VALUE')
+// 	})
+// 	.then(console.log)
+// console.log('MAIN PROGRAM')
+
+let promise = Promise.resolve('PROMISE VALUE')
+.then(console.log)
+.catch((error) => console.error('oh, you suck...'))
