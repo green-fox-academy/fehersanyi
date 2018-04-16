f18007847efda8455300bcb44576690423c8cb0d
fehersanyi <papahadacow@gmail.com>
2018-04-16 13:55:25 +0200

    task 5

--- week09/day1/proise/program.js
+++ week09/day1/proise/program.js
@@ -3,11 +3,7 @@
 require('es6-promise');
 
 let promise = new Promise((fulfill, reject) => {
-		fulfill('I FIRED');
-		reject(new Error('I DID NOT FIRE'))
+		fulfill('PROMISE VALUE')
 	})
-	.then(console.log, onRejected);
-
-function onRejected(error) {
-	console.log(error.message)
-}
+	.then(console.log)
+console.log('MAIN PROGRAM')
