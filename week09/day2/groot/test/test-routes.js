1c7a3ba4237449f936adb4a3994664c1cbcc6839
fehersanyi <papahadacow@gmail.com>
2018-04-17 12:50:20 +0200

    yondu is ready to fight

--- week09/day2/groot/test/test-routes.js
+++ week09/day2/groot/test/test-routes.js
@@ -1,3 +1,4 @@
+'use strict'
 
 const test = require('tape')
 const request = require('supertest')
@@ -9,11 +10,57 @@ test('groot endpoint', (t) => {
 		.expect(200)
 		.expect('Content-Type', /json/)
 		.expect({
-			received: "somemessage",
-			translated: "I am Groot!"
+			error: "I am Groot!"
 		})
 		.end((err, res) => {
 			t.error(err);
-			t.end();
 		});
-});
+
+	request(app)
+		.get('/groot?message=kakafej')
+		.expect(200)
+		.expect('Content-Type', /json/)
+		.expect({
+			recieved: "kakafej",
+			translated: "I am Groot"
+		})
+		.end((err, res) => {
+			t.error(err);
+		});
+		
+		request(app)
+		.get('/yondu')
+		.expect(200)
+		.expect('Content-Type', /json/)
+		.expect({
+			error: "no agression son"
+		})
+		.end((err, res) => {
+			t.error(err)
+			t.end();
+		})
+		
+		request(app)
+			.get('/yondu?distance=100&time=10')
+			.expect(200)
+			.expect('Content-Type', /json/)
+			.expect({
+				distance: "100",
+				speed: "10",
+				time: "10"
+			})
+			.end((err, res) => {
+				t.error(err);
+			});
+
+		request(app)
+			.get('/yondu?distance=kakafej&time=10')
+			.expect(200)
+			.expect('Content-Type', /json/)
+			.expect({
+				error: "you can't do this son"
+			})
+			.end((err, res) => {
+				t.error(err);
+			});
+	});
