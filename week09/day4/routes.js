904bc0c70e79d37e5d670ba0efff057dd54d46a4
fehersanyi <papahadacow@gmail.com>
2018-04-18 17:38:31 +0200

    done for today

--- week09/day4/routes.js
+++ week09/day4/routes.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const controll = require('./controll/controlData');
 
+app.use('/static', express.static('static'));
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 })
---
+++ week09/day4/static/app.js
@@ -0,0 +1,98 @@
+let one = document.querySelector('.one');
+let two = document.querySelector('.two');
+let three = document.querySelector('.three');
+let four = document.querySelector('.four');
+let theQuestion = document.querySelector('h2');
+let scoreTable = document.querySelector('header');
+let container = document.querySelector('.container');
+let score = 0;
+let rounds = 0;
+
+function writeQuestionOut(index) {
+  fetch('/questions')
+    .then((res) => res.json())
+    .then((question) => {
+      scoreTable.innerText = `SCORE: ${score}`
+      theQuestion.innerText = question[index].question;
+      one.innerText = question[index].answer;
+      two.innerText = question[index + 1].answer;
+      three.innerText = question[index + 2].answer;
+      four.innerText = question[index + 3].answer;
+    });
+}
+
+writeQuestionOut(rounds);
+
+one.addEventListener('click', () => {
+  fetchMyAnswer(one, rounds);
+});
+two.addEventListener('click', () => {
+  fetchMyAnswer(two, rounds + 1);
+});
+three.addEventListener('click', () => {
+  fetchMyAnswer(three, rounds + 2);
+});
+four.addEventListener('click', () => {
+  fetchMyAnswer(four, rounds + 3);
+});
+
+function fetchMyAnswer(button, index) {
+  fetch('/questions')
+    .then((res) => res.json())
+    .then((question) => {
+      if (question[rounds] !== undefined && question[rounds].is_correct !== undefined) {
+        if (question[index].is_correct === 1) {
+          button.classList.add('correct');
+          score++;
+          if (rounds <= question.length - 4) {
+            rounds += 4;
+          }
+        } else {
+          button.classList.add('incorrect')
+          if (rounds <= question.length - 4) {
+            rounds += 4;
+          }
+        }
+        seeYourScores()
+
+        setTimeout(() => {
+          reset();
+          writeQuestionOut(rounds);
+        }, 1000);
+      }
+    });
+};
+
+function reset() {
+  one.classList.remove('correct');
+  one.classList.remove('incorrect');
+  two.classList.remove('correct');
+  two.classList.remove('incorrect');
+  three.classList.remove('correct');
+  three.classList.remove('incorrect');
+  four.classList.remove('correct');
+  four.classList.remove('incorrect');
+}
+
+function ggwp() {
+
+  let finalScore = document.createElement('h1');
+  finalScore.setAttribute('class', 'final');
+  finalScore.innerText = `You scored ${score}/10`;
+  container.appendChild(finalScore);
+}
+let message = document.createElement('h1');
+message.innerText = 'Watch your score';
+
+message.addEventListener('click', () => {
+  container.removeChild(message);
+  ggwp()
+})
+
+function seeYourScores() {
+  if (rounds === 40) {
+    message.setAttribute('class', 'final');
+    message.classList.add('clickme');
+    container.appendChild(message);
+  }
+}
