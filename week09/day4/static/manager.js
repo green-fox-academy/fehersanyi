a83efe72f3af8b10ad0bf5000c687e3895fef399
fehersanyi <papahadacow@gmail.com>
2018-04-19 16:12:42 +0200

    frontend complete

--- week09/day4/static/manager.js
+++ week09/day4/static/manager.js
@@ -4,8 +4,20 @@ let deleterBox = document.querySelector('.deleter');
 fetch('/api/questions')
   .then((res) => res.json())
   .then((question) => {
-     console.log(question.length)
-      // for (let i = 0; i < question.length; i + 4) {
-      //   console.log(question[i])
-      // }
+    question.forEach((e, i) => {
+      if (i % 4 === 0) {
+        let actualQuestion = document.createElement('p');
+        actualQuestion.innerText = e.question;
+        actualQuestion.setAttribute('class', 'actualQuestion');
+        let trigger = document.createElement('p');
+        trigger.setAttribute('class', 'deletebutton');
+        trigger.innerText = 'delete';
+        let questionHolder = document.createElement('div');
+        questionHolder.setAttribute('class', 'questionHolder')
+        actualQuestion.appendChild(trigger);
+        questionHolder.appendChild(actualQuestion);
+        deleterBox.appendChild(questionHolder);
+      }
     });
+
+  });
